8611931712be4f5454426876e30b762d2a0a50ef
VijayKadiyam <freelancervijayk@gmail.com>
2019-03-03 01:32:05 +0530

    Drop down headings added 1

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,24 +28,13 @@
         <!-- With dropdowns -->
         <v-list-group
           v-for="(dropdown, d) in dropdownItems"
-          :key="`D${d}`"
+          :key="d"
           :prepend-icon="dropdown.icon"
           no-action
         >
           <v-list-tile slot="activator">
             <v-list-tile-title>{{ dropdown.name }}</v-list-tile-title>
           </v-list-tile>
-
-          <!-- <v-list-tile
-            v-for="(item, i) in dropdown.items"
-            :key="i"
-            :to="item.to"
-          >
-            <v-list-tile-action>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content v-text="item.title"></v-list-tile-content>
-          </v-list-tile> -->
         </v-list-group>
       </v-list>
     </v-navigation-drawer>
