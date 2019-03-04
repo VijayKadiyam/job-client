f42d3412838c3548bc9774f0a028a8b061a4740c
VijayKadiyam <freelancervijayk@gmail.com>
2019-03-04 14:15:29 +0530

    testing only list 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,93 +1,9 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      :class="`${baseColor} lighten-5`"
-      :clipped="clipped"
-      :value="drawer"
-      fixed
-      app
-      width=225
-    >
-      <v-list>
-        <!-- Without dropdowns -->
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <!-- With dropdowns -->
-        <v-list-group
-          v-for="(dropdown, d) in dropdownItems"
-          :key="`D${d}`"
-          :prepend-icon="dropdown.icon"
-          no-action
-        >
-          <template slot:activator>
-            <v-list-tile>
-              <v-list-tile-title>{{ dropdown.name }}</v-list-tile-title>
-            </v-list-tile>
-          </template>
-
-          <v-list-tile
-            v-for="(item, i) in dropdown.items"
-            :key="i"
-            :to="item.to"
-          >
-            <v-list-tile-action>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content v-text="item.title"></v-list-tile-content>
-          </v-list-tile>
-        </v-list-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-      :height="baseHeight"
-      :color="baseColor"
-      :dark="darkStatus"
-    >
-      <nuxt-link to="/">
-        <v-icon large right>account_balance</v-icon>
-        <span style="color: white"> {{ title }}</span>
-      </nuxt-link>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <template v-if="authenticated">
-          <v-btn flat to="/dashboard">
-            Dashboard
-            <v-icon dark right>dashboard</v-icon>
-          </v-btn>
-          <v-btn flat @click="logout" >
-            Logout
-            <v-icon dark right>cancel_presentation</v-icon>
-          </v-btn>
-        </template>
-        <template v-else>
-          <v-btn flat to="/auth/register">
-            Register
-            <v-icon dark right>add_circle</v-icon>
-          </v-btn>
-          <v-btn flat to="/auth/login">
-            Login
-            <v-icon dark right>forward</v-icon>
-          </v-btn>
-        </template>
-      </v-toolbar-items>
-    </v-toolbar>
     <v-content>
+      <v-container px-0>
+        HI
+      </v-container>
     </v-content>
     <!-- <v-footer
       :fixed="fixed"
