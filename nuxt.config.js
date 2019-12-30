5fa8e47a20c064c98ed95a913386fd8c5ed74d10
VijayKadiyam <freelancervijayk@gmail.com>
2019-12-30 18:15:17 +0530

    LAtest commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,8 +93,9 @@ module.exports = {
   // },
   axios: {
     // baseURL: 'http://192.168.1.9:8080/api'
-    baseURL: 'http://192.168.1.126:8080/api'
+    // baseURL: 'http://192.168.1.126:8080/api'
     // baseURL: 'http://aaibuzz.com:8088/api'
+    baseURL: 'http://api.digiloop.aaibuzz.com/api'
   },
   
 
